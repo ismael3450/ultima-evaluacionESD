43c38c716d3a64da0702a6248562959799b22af0
Ismael <ismaelalexanderm4@gmail.com>
2026-06-09 15:37:54 -0600

    Configuracion inicial del proyecto

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+mod graph_core;
+mod data_loader;
+mod ui;
+mod algorithms;
+
+fn main() {
+    println!("--- Sistema de Monitoreo de Red Inicializado ---");
+
+    // Aquí se conectará el flujo principal más adelante:
+    // let datos = data_loader::inicializar_datos();
+    // let grafo = graph_core::construir_grafo(datos);
+
+    // Así los llamarán en el main:
+    // algorithms::bfs::ejecutar_bfs(&grafo);
+    // algorithms::dfs::ejecutar_dfs(&grafo);
+}
